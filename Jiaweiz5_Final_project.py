98f9f6503b545545cdad594e0b5ad0add9b41673
vivizhao95 <zjw1104157298@me.com>
2020-05-02 22:47:33 -0500

    import files & hyphothesis 1 (50%)

--- Jiaweiz5_Final_project.py
+++ Jiaweiz5_Final_project.py
@@ -33,6 +33,7 @@ https://data.world/oecd/meat-consumption
 import pandas as pd
 import numpy as np
 
+
 def avgChRateOfCountries(df , num_of_country, start_point):
     """
 
@@ -47,6 +48,15 @@ def avgChRateOfCountries(df , num_of_country, start_point):
         avg_ch_rate.append(val)
 
     return avg_ch_rate
+
+
+def stringToFloat(list):
+    for i in range(0, len(list)):
+        if type(list[i]) == str:
+            list[i] = float(list[i].replace(',', ''))
+    return list
+
+
 # import data
 forest_cov = pd.read_csv('Forest_land.csv')
 agriculture_land = pd.read_csv('Agriculture_land.csv')
@@ -55,9 +65,39 @@ metadata_forest_cov = pd.read_csv('Metadata_forest.csv')
 GDP = pd.read_csv('GDP.csv')
 food_consumption = pd.read_csv('InternationalFoodConsumption.csv')
 
-#Hypothesis 1: there would be a negative correlation between the agriculture land hold by a country and the forest coverage.
+# Hypothesis 1
+income = metadata_agriculture['IncomeGroup'].tolist()
+income = pd.DataFrame(income, columns = ['Country Income'])
 
+country_name = agriculture_land.T.iloc[0].tolist()
+df_country_name = pd.DataFrame(country_name, columns = ['Country Name'])
 
 avg_ch_rate_agriculture = avgChRateOfCountries(agriculture_land , 263, 4)
+df_agr = pd.DataFrame(avg_ch_rate_agriculture, columns = ['slope_agr'])
 
-print(avg_ch_rate_agriculture)
+country_agr = pd.concat([df_country_name, df_agr],axis=1, join='inner')
+df = pd.concat([df_country_name, income, df_agr],axis=1, join='inner')
+
+
+# Hypothesis 2 (GDP in 2010)
+
+gdp_str = GDP.set_index('year').iloc[-3].tolist()
+gdp = stringToFloat(gdp_str)
+gdp_2008 = pd.DataFrame(gdp, columns = ['GDP_2008'])
+gdp_list = list(GDP)[1:]
+gdp_list = [x.rstrip(' ') for x in gdp_list]
+
+df_gdp = pd.DataFrame(gdp_list, columns = ['Country Name'])
+
+df_gdp = pd.concat([df_gdp, gdp_2008],axis=1).dropna()
+df_gdp.columns = df_gdp.columns.str.rstrip()
+df = pd.merge(df, df_gdp, how = 'inner', on = ['Country Name'])
+avg_ch_rate_for = avgChRateOfCountries(forest_cov, 263,4)
+df_for = pd.DataFrame(avg_ch_rate_for, columns = ['slope_forest'])
+for_2008 = forest_cov['2008'].tolist()
+forest_2008 = pd.DataFrame(for_2008, columns = ['Forest_2008_% of land area'])
+df_merge = pd.concat([df_country_name, income,df_agr, df_for, forest_2008],axis=1, join='inner')
+
+df_1 = pd.merge(df_merge, df_gdp, how = 'inner', on = ['Country Name'])
+df_final = df_1.sort_values('GDP_2008')
+print(df_final)
