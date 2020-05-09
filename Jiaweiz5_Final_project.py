9a760e9826d003fbbca77e24e711f242b8543b0f
vivizhao95 <57853634+vivizhao95@users.noreply.github.com>
2020-05-09 18:11:13 -0500

    Update Jiaweiz5_Final_project.py

--- Jiaweiz5_Final_project.py
+++ Jiaweiz5_Final_project.py
@@ -1,35 +1,3 @@
-"""
-Title: Analyse Potential Factors Affecting Forest Coverage of Different Countries
-
-team member: Jiawei Zhao
-
-Github ID: vivizhao95
-
-Hypothesis 1: there would be a negative correlation between the  agriculture land hold by a country and the forest coverage.
-
-Hypothesis 2: there would be a negative correlation between the GDP and the forest coverage of a country.
-
-Hypothesis 3: if a country consumes more meat per capita, the country would have a lower forest coverage. (livestock requires agriculture lands)
-
-source:
-
-1. Agricultural land (% of land area)
-
-https://data.worldbank.org/indicator/ag.lnd.frst.zs
-
-2. Forest land (% of land area)
-
-https://data.worldbank.org/indicator/AG.LND.AGRI.ZS
-
-3. GDP by country
-
-https://www.kaggle.com/jtennis/gdp-by-country-the-maddisonproject/data
-
-4. Meat Consumption by country
-
-https://data.world/oecd/meat-consumption/workspace/file?filename=meat_consumption.csv
-
-"""
 import pandas as pd
 import matplotlib.pyplot as plt
 
